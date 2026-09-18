63690d3d248b727c18529299dead34ae13bf51f2
sgatlantis-seo <sgatlantis.seo@gmail.com>
2026-09-18 04:47:21 +0000

    Refactor code structure for improved readability and maintainability

--- flights.py
+++ flights.py
@@ -7,3 +7,8 @@ flights_pivot = flights.pivot(index="month", columns="year", values="passengers"
 
 sns.barplot(x="year", y="passengers", data=flights_year)
 plt.savefig("flights_year_barplot.png")
+
+sns.heatmap(flights_pivot, cmap="YlGnBu", annot=True, fmt="d")
+plt.savefig("flights_pivot_heatmap.png")
+
+print(flights_year.corr())
